f45e8226e48e3eaa3a4295c12314450f66917fed
iam-mynuddin <mynuddin.shaik234@gmail.com>
2024-04-05 11:24:43 +0530

    Using async pipe - to unsubscribe from user subscriptions

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -19,6 +19,7 @@ export class AccountService {
         const user = response;
         if (user) {
           localStorage.setItem('user', JSON.stringify(user));
+          this.currentUserSource.next(user);
         }
       })
     )
